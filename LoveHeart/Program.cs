587ddb2d4dce19e1ba405f0b6fa85451c8fb063d
Robert Tublen <robert.tublen@junction7.se>
2018-10-06 10:37:10 +0200

    Add pseudo code

--- LoveHeart/Program.cs
+++ LoveHeart/Program.cs
@@ -6,7 +6,86 @@ namespace LoveHeart
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Define all our views
+            var loginView = new LoginView();
+            var receptionistMainView = new ReceptionistMainView();
+            var veterinaryMainView = new VeterinaryMainView();
+            // adminview
+            // add pet view
+            // .. 
+
+            var input = loginView.GetInput();
+
+            ConsoleView activeView;
+
+            switch (input)
+            {
+                case "receptionistView":
+
+                    activeView = receptionistMainView;
+                    break;
+
+                case "receptionistView":
+
+                    activeView = veterinaryMainView;
+
+                    break;
+
+                default:
+
+                    activeView = loginView;
+
+                    break;
+
+            }
+
+            activeView.Dislay();
+        }
+
+        class LoginView : ConsoleView
+        {
+            public override void Display()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void GetInput()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class ReceptionistMainView : ConsoleView
+        {
+            public override void Display()
+            {
+                throw new NotImplementedException();
+            }
+            public override void GetInput()
+            {
+                throw new NotImplementedException();
+            }
+
+        }
+
+        class VeterinaryMainView : ConsoleView
+        {
+            public override void Display()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void GetInput()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        abstract class ConsoleView
+        {
+            public abstract void Display();
+
+            public abstract void GetInput();
         }
     }
 }
